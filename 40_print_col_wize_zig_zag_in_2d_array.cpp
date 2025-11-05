7e0eb8b4791c454ad9ae3a32ce0cffcc0c562bbd
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-11-05 14:10:52 +0530

    Create 40_print_col_wize_zig_zag_in_2d_array.cpp

---
+++ 40_print_col_wize_zig_zag_in_2d_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    int arr[4][4]={{3,1,5,6}, {4,2,1,3}, {5,1,3,7}, {9,8,1,8}};
+
+    for(int i=0; i<4; i++){
+        if(i%2 == 0){
+            for(int j=0; j<4; j++){
+                cout << arr[j][i]<< " ";
+    
+            }
+            cout << endl;
+        }
+        else{
+            for(int j=3; j>=0; j--){
+                cout << arr[j][i]<< " ";
+                
+            }
+            cout << endl;
+        }
+        
+    }
+    // cout << "hey";
+    return 0;
+}
