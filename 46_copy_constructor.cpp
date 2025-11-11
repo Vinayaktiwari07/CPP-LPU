cedbc9254551c4f9d816af12aa03d3d23bb0b20a
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-11-11 14:05:11 +0530

    Create 46_copy_constructor.cpp

---
+++ 46_copy_constructor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+public:
+    int age;
+    string name;
+    // default constructor
+    // Student(){
+    //     // age = 12;
+    // }
+
+    Student (int ageOfStudent = 20,string nameOfStudent = "Unknown" ){
+        age = ageOfStudent;
+        name = nameOfStudent;
+    }
+    student(const student &s){
+        age = s.age;
+        
+    }
+};
+int main() {
+    // Student s;
+
+    // s.roll_no = 12;
+    // s.age = 11;
+
+    // cout << s.roll_no << endl;
+    // cout << s.age << endl;
+
+    Student s1(23, "Vinayak");
+    // Student s2("anil");
+    Student s3;
+
+    cout << s1.age << endl;;
+    cout << s1.name << endl;
+    cout << s3.age << endl;
+    
+    return 0;
+}
